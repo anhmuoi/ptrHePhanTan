440d5b1e36124ed4a731749a8be8401f1e5a3165
annt <nguyenthaian1221@gmail.com>
2023-04-20 18:08:16 +0700

    an'commit: add textInputModal

---
+++ src/components/common/textInputModal.js
@@ -0,0 +1,35 @@
+import React, { Component, useState } from 'react';
+
+const TextInputModal = (props) => {
+    const [text, setText] = useState('');
+    const handleOnClick = () => {
+        props?.onSaveChanges(text);
+        props.onClose();
+    };
+    let activeClass = '';
+    if (props?.show) {
+        activeClass = 'is-active';
+    }
+
+    return (
+        <div className={`modal  ${activeClass}`}>
+            <div className="modal-background" />
+            <div className="modal-card">
+                <header className="modal-card-head">
+                    <p className="modal-card-title">Add Text</p>
+                    <button className="delete" aria-label="close" onClick={props.onClose} >x</button>
+                </header>
+                <section className="modal-card-body">
+                    <textarea className="textarea" type="text" onChange={(e) => setText(e.target.value)} />
+                </section>
+                <footer className="modal-card-foot">
+                    <button className="button is-success" onClick={() => handleOnClick()}>
+                        Save changes
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+};
+
+export default TextInputModal;
